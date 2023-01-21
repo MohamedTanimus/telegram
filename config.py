50d0e24307c7c53b549249b123e7735b7bbb4977
MohamedTanimus <91375689+MohamedTanimus@users.noreply.github.com>
2023-01-21 20:29:36 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-BOT_TOKEN = "5086218396:AAHAj4Gu7lDM8IZToQ6oRmtsUnNgRPSmT7M"
-APP_URL = "https://appitele.herokuapp.com/" + BOT_TOKEN
+BOT_TOKEN = "5934952771:AAHsnRP_5xHqAgFqKxXTOw6SC9TsagGVzKM"
+APP_URL = "https://tgbbbit.herokuapp.com/" + BOT_TOKEN
